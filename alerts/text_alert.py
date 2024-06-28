1df4e633933255fce2cc8cb7f792872c700b07dc
Sahil Ali <79185551+SahilAli3628@users.noreply.github.com>
2024-06-28 12:33:21 +0530

    Update text_alert.py

--- alerts/text_alert.py
+++ alerts/text_alert.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 def text_alert(msg):
-	client = Client("AC5be4774cd9efd92f16f757c83abbf719", "604a168faa92f2551821a81e9302ceaa")
+	client = Client("<key1>", "<key2>")
 
-	client.messages.create(to = ["<mobile number with country code>"], from_ = "+16065352966", body = msg)
+	client.messages.create(to = ["<mobile number with country code>"], from_ = "<dummy client number>", body = msg)
 
 
